ca1e76212aed9df18d78dce478bc21d13bff2c05
Moon <5924350+MatrikMoon@users.noreply.github.com>
2024-08-23 23:40:04 -0500

    Don't worry, that token was meaningless >~>

--- RelayServer/Standard WebSocket/src/settings.ts
+++ RelayServer/Standard WebSocket/src/settings.ts
@@ -20,7 +20,7 @@ export default {
     TA: {
         ip: 'dev.tournamentassistant.net',
         port: '8676',
-        token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjY2MTZCMTZDOEE2NjNBRkU2REI3NjY0OEExQ0UyNTc2NDFFM0IyMDciLCJ4NXQiOiJaaGF4YklwbU92NXR0MlpJb2M0bGRrSGpzZ2MiLCJ0eXAiOiJKV1QifQ.eyJpYXQiOiIxNzI0NDcwMDc1IiwiZXhwIjoiMTcyNDkwMjA3NSIsInRhOmRpc2NvcmRfaWQiOiIyMjk0MDg0NjU3ODc5NDQ5NzAiLCJ0YTpkaXNjb3JkX25hbWUiOiJtYXRyaWttb29uIiwidGE6ZGlzY29yZF9hdmF0YXIiOiJhX2ZkOWExNTFjZTcwNWQzYWE0Nzk3MzE5MDI3ZmFkYmI5IiwiaXNzIjoidGFfc2VydmVyIiwiYXVkIjoidGFfdXNlcnMifQ.flTQOD9OazKMT9IX90h3sPmBOaqp-zbRQVCc4eL1GA_4qPl5jyZRbz6F4eu1dCR5amQIZ-S66bwp0_4Vl2K1-UB4srMVCeN4RklkPh5yv2JgQMgE1zge4FdpIobVk7IHvkY23xwfsetU37UxQ6FXOr0cSTZ2aHwk8DYqu_0RqrZUotO7CjVZbnstx1GYUQmK2dd8AMRi0vO-Rt2GTZI72DKuXyWnQIg1kGOl3uo1DworEZVw98Mm9lJm79LMUMqltdO4OsjAAoDb8A9E3I1D0Sbq6n-_ftl97JuycikjjDv0I4vVBXtSTjMPYT9qIGTj2mi6YXxVxjaYpfdQ4FA2gw',
+        token: 'readonly',
         tourneyName: 'Moon\'s Test Tourney'
     },
     Gamemode: "VERSUS",
